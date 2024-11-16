341948641143d1582a3406d1d8e58103b566eddf
Clement Callejon <9397@holbertonstudents.com>
2024-11-16 10:11:13 +0100

    Added _ to all floors

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,11 +3,11 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors){
     super(sqft);
-    this.floors = floors;
+    this._floors = floors;
   }
 
   get floors(){
-    return this.floors;
+    return this._floors;
   }
   
   set floors(value){
