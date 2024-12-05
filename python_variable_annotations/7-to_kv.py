1434445d99cf160426d5fb44aa5e2a78c2299e58
Clement Callejon <9397@holbertonstudents.com>
2024-12-05 16:44:36 +0100

    Changed code to use 'typing' module as it's apparently required by the checker and never mentionned

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import Union
+""" Module that contains a 'to_kv' function """
+
+
+def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+    """ Returns a tuple of its arguments and squares the second one """
+    return (k, v ** 2)
