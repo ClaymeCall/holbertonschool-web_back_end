3a5a03477811ac9b577ea36f88509d5648947211
Clement Callejon <9397@holbertonstudents.com>
2024-12-05 08:30:47 +0100

    Added task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Module that contains a 'sum_list' function """
+
+
+def sum_list(input_list: list[float]) -> float:
+    """ Returns the sum of a list of floats as float """
+    return sum(input_list)
