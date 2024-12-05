68b0f99bf8fcbb5b12ce0abbefdce58a83b262a9
Clement Callejon <9397@holbertonstudents.com>
2024-12-05 08:33:17 +0100

    Added task 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Module that contains a 'sum_mixed_list' function """
+
+
+def sum_mixed_list(mxd_lst: list[int | float]) -> float:
+    """ Returns the sum of a list of int/float as float """
+    return sum(mxd_lst)
