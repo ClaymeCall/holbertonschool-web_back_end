690c820af6bbc8d0d76e390e5cba167d5688dae0
Clement Callejon <9397@holbertonstudents.com>
2024-12-06 19:15:34 +0100

    Coding style

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -10,8 +10,8 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 def measure_time(n: int, max_delay: int) -> float:
     """ Measures the execution time for wait_n """
 
-    start_time = time.time() # Log start time
-    asyncio.run(wait_n(n, max_delay)) # Execute function
-    end_time = time.time() # Log end time
+    start_time = time.time()  # Log start time
+    asyncio.run(wait_n(n, max_delay))  # Execute function
+    end_time = time.time()  # Log end time
 
-    return end_time - start_time # Return total execution time
+    return end_time - start_time  # Return total execution time
