86a76b18d1183aaacff1a917cf93603521707819
Clement Callejon <9397@holbertonstudents.com>
2024-12-06 17:16:25 +0100

    Pycodestyle

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """ Module that contains a 'sum_mixed_list' function """
 
+
 from typing import Union, List
+
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ Returns the sum of a list of int/float as float """
