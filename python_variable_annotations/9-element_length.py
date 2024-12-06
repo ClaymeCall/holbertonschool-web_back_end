82258cd8a93c5619a793c3f358a0917c0f480cae
Clement Callejon <9397@holbertonstudents.com>
2024-12-06 17:12:28 +0100

    Moved imports after module documentation

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-from typing import Iterable, Sequence, List, Tuple
 """ Module that contains a 'element_length' function"""
+
+
+from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
