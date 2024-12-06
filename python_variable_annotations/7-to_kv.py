18e608cc9ac762386f180899bff7d0b0b09752b1
Clement Callejon <9397@holbertonstudents.com>
2024-12-06 17:09:03 +0100

    Added task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import Union
+from typing import Union, Tuple
 """ Module that contains a 'to_kv' function """
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ Returns a tuple of its arguments and squares the second one """
     return (k, v ** 2)
