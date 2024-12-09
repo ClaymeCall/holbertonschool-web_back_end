e91d865f8031f9c0cfdc6ab0495b49182357f0f6
Clement Callejon <9397@holbertonstudents.com>
2024-12-09 14:56:26 +0100

    pycodestyle

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,8 +4,9 @@
 import random
 import asyncio
 
+
 async def async_generator():
     """ Yields 10 random numbers after waiting 1 sec for each """
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0,10)
+        yield random.uniform(0, 10)
