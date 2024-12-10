b12ae7bfdbc8f999758faff23ddb22d7d50636c6
Clement Callejon <9397@holbertonstudents.com>
2024-12-10 14:09:47 +0100

    Pycodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,6 +3,7 @@
 
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ Returns the page range base on current and scope size """
     first_elem_of_page = page_size * (page - 1)
