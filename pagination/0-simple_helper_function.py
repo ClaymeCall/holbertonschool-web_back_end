c90b5a34ca034e3aeeccad0416b2a5a25525d12c
Clement Callejon <9397@holbertonstudents.com>
2024-12-10 14:08:49 +0100

    Added pagination helper function

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" This module contains a 'index_range' function """
+
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ Returns the page range base on current and scope size """
+    first_elem_of_page = page_size * (page - 1)
+    last_elem_of_page = first_elem_of_page + page_size
+
+    return (first_elem_of_page, last_elem_of_page)
