3ea6b0c0384f9bc477ab9134811e0e696b78b826
Clement Callejon <9397@holbertonstudents.com>
2025-01-24 21:08:47 +0100

    Eslint fixes

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,18 +2,17 @@
 const process = require('process');
 
 // Prompt the user to input text
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Get user input
 process.stdin.on('readable', () => {
-	const name = process.stdin.read();
-	if (name) {
-		process.stdout.write(`Your name is: ${name}`);
-	}
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
 // Print a message upon closing the input stream
 process.stdin.on('end', () => {
-	console.log('This important software is now closing');
+  console.log('This important software is now closing');
 });
-
--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -4,33 +4,31 @@
 const fs = require('fs');
 
 const countStudents = (path) => {
-	try {
-		const data = fs.readFileSync(path, 'utf8'); // Store the files data in a variable
-		const lines = data.split('\n').filter((line) => line);// Split by line, and filter any empty one
-		const fields = {};
-		lines.forEach((line) => {
-			const student = line.split(','); // Split the data by commas
+  try {
+    const data = fs.readFileSync(path, 'utf8'); // Store the files data in a variable
+    const lines = data.split('\n').filter((line) => line);// Split by line, and filter any empty one
+    const fields = {};
+    lines.forEach((line) => {
+      const student = line.split(','); // Split the data by commas
 
-			// If the current field is not in the list, add it
-			if (!fields[student[3]]) fields[student[3]] = [];
-			fields[student[3]].push(student[0]);
-		});
+      // If the current field is not in the list, add it
+      if (!fields[student[3]]) fields[student[3]] = [];
+      fields[student[3]].push(student[0]);
+    });
 
-		// Log the count of students
-		console.log(`Number of students: ${lines.length - 1}`);
+    // Log the count of students
+    console.log(`Number of students: ${lines.length - 1}`);
 
-		// Print the list of students in each field
-		for (const field in fields) {
-			if (field) {
-				const list = fields[field].join(', ');
-				console.log(`Number of students in ${field}: ${fields[field].length}. List: ${list}`);
-			}
-		}
-	} catch (error) {
-		throw new Error('Cannot load the database');
-	}
+    // Print the list of students in each field
+    for (const field in fields) {
+      if (field) {
+        const list = fields[field].join(', ');
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${list}`);
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 };
 
 module.exports = countStudents;
-
-
